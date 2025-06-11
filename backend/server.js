89f03e0dd1515ba182a838a96690d6e9bdf7a50a
aravindpulluri116 <pulluriaravind@gmail.com>
2025-06-11 21:21:02 +0530

    push

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:8080'],
+  origin: 'http://localhost:8080',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
