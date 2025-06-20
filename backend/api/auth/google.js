8817c4430454bb063ab763085143f5b41169bb5e
aravindpulluri116 <pulluriaravind@gmail.com>
2025-06-20 15:46:32 +0530

    4

--- backend/api/auth/google.js
+++ backend/api/auth/google.js
@@ -1,1 +1,9 @@
- 
+export default function handler(req, res) {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const redirectUri = encodeURIComponent("https://foodieshubbackend.vercel.app/api/auth/google/callback");
+  const scope = encodeURIComponent("openid email profile");
+
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`;
+
+  res.redirect(302, authUrl);
+} 
